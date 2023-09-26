f69c7c0d9129239cda25bb31d589eb31c69858cd
timnilsson04 <timnils04@gmail.com>
2023-09-26 14:37:23 +0200

    🎉

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import './Spinner.css'
+
+function Spinner() {
+    return (
+        <>
+            <h1 className='loader'></h1>
+            <h2>loading...</h2>
+        </>
+    )
+}
+
+export default Spinner
